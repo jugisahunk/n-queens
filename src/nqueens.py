41da9ed99e3fc74dd8f078f6fb39db2d2c33b9ae
Jason Knight <jugisahunk@gmail.com>
2022-09-04 20:19:24 -0500

    added 2 queens and gitignore

--- src/nqueens.py
+++ src/nqueens.py
@@ -1,2 +1,6 @@
-def placequeens():
-    print("Hello World!")
+def placequeens(n):
+    rows = [0 for i in range(n)] 
+    columns = [0 for i in range(n)] 
+    board = [rows,columns]
+
+    return board
--- tests/nqueenstest.py
+++ tests/nqueenstest.py
@@ -3,5 +3,11 @@ from src.nqueens import placequeens
 
 class n_queens_test(unittest.TestCase):
     def test_place_one_queen(self):
-        board = [[0],[0]]
-        self.assertEqual(board, [[0],[0]])
+        expected_board = [[0],[0]]
+        actual_board = placequeens(1)
+        self.assertEqual(expected_board, actual_board)
+
+    def test_place_two_queens(self):
+        expected_board = [[0,0],[0,0]]
+        actual_board = placequeens(2)
+        self.assertEqual(expected_board, actual_board)
