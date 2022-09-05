39a5d4812a96e0840b1616d651de967ba2ba59b8
Jason Knight <jugisahunk@gmail.com>
2022-09-05 12:29:41 -0500

    redesigned tests

--- src/nqueens.py
+++ src/nqueens.py
@@ -1,6 +1,6 @@
 def placequeens(n):
-    rows = [0 for i in range(n)] 
-    columns = [0 for i in range(n)] 
-    board = [rows,columns]
+    rows, cols = (n, n)
+
+    board = [['X' for i in range(cols)] for j in range(rows)]
 
     return board
--- tests/nqueenstest.py
+++ tests/nqueenstest.py
@@ -2,12 +2,23 @@ import unittest
 from src.nqueens import placequeens
 
 class n_queens_test(unittest.TestCase):
+    def pretty_print_board(self, board):    
+        print('print board')
+        print(board)    
+        for row in board:
+            print(''.join(row))
+    
     def test_place_one_queen(self):
-        expected_board = [[0],[0]]
+        expected_board = [['X']]
         actual_board = placequeens(1)
         self.assertEqual(expected_board, actual_board)
 
     def test_place_two_queens(self):
-        expected_board = [[0,0],[0,0]]
+        rows = []
+        expected_board = [['X','X'],['X','X']]
         actual_board = placequeens(2)
+
         self.assertEqual(expected_board, actual_board)
+
+    # def test_place_4_queens(self):
+    #     expected_board = [[],[]]]
